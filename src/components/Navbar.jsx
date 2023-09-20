f31bc8ec666f9729b299ac83d3d7645d517fb9e4
Olumide Odebiyi <oluodebiyi@gmail.com>
2023-09-20 16:41:20 +0100

    update styled components for Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import { Search } from '@mui/icons-material';
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+height: 60px;
+
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+`;
+
+const Left = styled.div`
+flex: 1;  
+display: flex;
+align-items: center;  
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgrey;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+ border: none;
+
+`
+
+const Center = styled.div`
+  flex: 1;  
+`
+
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
+const Right = styled.div`
+  flex: 1;  
+`
+
+const Navbar = () => {
+    return (
+        <Container>
+           <Wrapper> 
+                <Left> 
+                    <Language>ENG</Language>
+                    <SearchContainer>
+                       <Input />
+                     <Search />
+                       
+                    </SearchContainer>
+                </Left>
+                <Center> 
+                    <Logo>ShoppersLeague</Logo>
+                </Center>
+                <Right> Right </Right>
+                
+            </Wrapper> 
+        </Container>
+    )
+}
+
+export default Navbar
