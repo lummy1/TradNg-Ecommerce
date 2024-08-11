46fc32a49aeaf79332ec786470d44f993e7d1c0d
Olumide Odebiyi <oluodebiyi@gmail.com>
2024-08-11 02:02:35 +0100

    changed data js directory

---
+++ client/src/components/data.js
@@ -0,0 +1,86 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://moloyal.com/trads/banner1.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=1000",
+      title: "NEW ARRIVALS",
+      desc: "Sleek Men!!! GET 30% OFF ON NEW ARRIVALS",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://moloyal.com/trads/banner2.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=1000",
+      title: "POPULAR SALES",
+      desc: "Have a feel of our MOST Popular Items at a discount",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://moloyal.com/trads/banner3.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=1000",
+      title: "MEN ACCESSORIES",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR MEN ACCESORIES.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://moloyal.com/trads/ladytrad4.jpg?auto=compress&cs=tinysrgb&dpr=2&h=500&w=400",
+      title: "WEDDING GLAM",
+      cat: "wedding"
+    },
+    {
+      id: 2,
+      img: "https://moloyal.com/trads/outfit_cat.jpg?auto=compress&cs=tinysrgb&dpr=2&h=500&w=400",
+      title: "PARTY TRADS",
+      cat: "trads"
+    },
+    {
+      id: 3,
+      img: "https://moloyal.com/trads/ladytrad2.jpg?auto=compress&cs=tinysrgb&dpr=2&h=500&w=400",
+      title: "TRAD ACCESSORIES",
+      cat: "accessories"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      title: "nike tshirt",
+      img:"https://moloyal.com/trads/couple7.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:2,
+      title: "nike tshirt",
+      img:"https://avantesoft.com/trads/couple4.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:3,
+      img:"https://avantesoft.com/trads/couple3.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:4,
+      title: "adidas tshirt",
+      img:"https://avantesoft.com/trads/couple1.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:5,
+      title: "puma tshirt",
+      img:"https://ecoms.imgix.net/banner/couple5.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:6,
+      title: "fubu tshirt",
+      img:"https://ecoms.imgix.net/banner/couple6.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:7,
+      title: "allstars tshirt",
+      img:"https://ecoms.imgix.net/banner/couple9.png?&auto=format,enhance,compress&q=60",
+    },
+    {
+      id:8,
+      title: "tommy tshirt",
+      img:"https://ecoms.imgix.net/banner/couple8.png?&auto=format,enhance,compress&q=60",
+    },
+  ]
